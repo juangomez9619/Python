88076a38b5a2c3535968a24d33067b2451358b43
juangomez9619 <judgomezb@correo.udistrital.edu.co>
2020-09-15 18:45:58 -0500

    Sorting a dictionary using tuples

--- Basics/tuples.py
+++ Basics/tuples.py
@@ -24,4 +24,31 @@ inventario = counts.items()  # list of tuples
 
 # tuples are comparable
 
-print((1, 2) < (3, 4))
+print((1, 2) < (3, 4)) #priority to first item
+
+
+# list of tuples
+d = {'a':10,
+     'b':1,
+     'c':22}
+
+#  sorted works based by the key
+for key, value in sorted(d.items()):
+    print(key, value)
+
+c = {'a': 10,
+     'b': 1,
+     'c': 22}
+
+tmp = list()
+for key, value in c.items():
+    tmp.append((value, key))
+
+print(tmp)
+
+tmp = sorted(tmp, reverse=True) #descending order
+print(tmp)
+
+e = {'a':13, 'b':2, 'c': 20}
+
+print(sorted([(v, k) for k, v in e.items()], reverse=True))
