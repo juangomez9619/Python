4376048a802112b1f5b4d31c189173b3219f241a
juangomez9619 <judgomezb@correo.udistrital.edu.co>
2020-09-15 22:37:33 -0500

    Update Regular_expressions.py

--- Basics/Regular_expressions.py
+++ Basics/Regular_expressions.py
@@ -9,3 +9,18 @@ Line = "I am From Colombia"
 if re.search('^From', Line):
     print(Line)
 
+#  search returns True False
+
+# [0-9]+ one or more digits
+
+x = "My favorite numbers are 14 and 1302 and 1"
+y = re.findall('[0-9]+', x)
+print(y)
+
+# ^F.+:
+
+# '^From (\S+@\S+)'
+
+text = 'From juan.gomez@gmail.com at blablabla'
+my_email = re.findall('^From (\\S+@\\S+)', text)
+print(my_email)
