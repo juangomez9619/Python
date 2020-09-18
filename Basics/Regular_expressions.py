95488693c68ab5bea3ac74cb7bb2dc42671e7905
juangomez9619 <judgomezb@correo.udistrital.edu.co>
2020-09-17 19:24:58 -0500

    Update Regular_expressions.py

--- Basics/Regular_expressions.py
+++ Basics/Regular_expressions.py
@@ -6,8 +6,8 @@ Line = "I am From Colombia"
 # . any character
 # * different characters:
 # \S any character except whitespace:
-if re.search('^From', Line):
-    print(Line)
+#if re.search('^From', Line):
+    #print(Line)
 
 #  search returns True False
 
@@ -15,7 +15,7 @@ if re.search('^From', Line):
 
 x = "My favorite numbers are 14 and 1302 and 1"
 y = re.findall('[0-9]+', x)
-print(y)
+#print(y)
 
 # ^F.+:
 
@@ -23,5 +23,29 @@ print(y)
 
 text = 'From juan.gomez@gmail.com at blablabla'
 my_email = re.findall('^From (\\S+@\\S+)', text)
-print(my_email)
+#print(my_email)
+
+hand = ["X-DSPAM-Confidence: 0.2345 ",
+        " X-DSPAM-Confidence: 0.15 ",
+        " 0.2345 ",
+        " X-DSPAM-Confidence: 1.235 "]
+
+num_list = []
+for line in hand:
+    line = line.strip()  # remove white spaces at the beginning and the end
+    interest_value = re.findall('^X-DSPAM-Confidence: ([0-9.]+)', line)  # stores one item
+    if len(interest_value) != 1:
+        continue
+    num_list.append(float(interest_value[0]))
+print(max(num_list))
+
+
+
+
+
+
+
+
+
+
 
