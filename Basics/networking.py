51d78ca07146ad48d3515822c4c53ff3b9df5517
juangomez9619 <judgomezb@correo.udistrital.edu.co>
2020-09-18 11:20:36 -0500

    Project_created

--- Basics/networking.py
+++ Basics/networking.py
@@ -2,3 +2,9 @@ import socket
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('data.pr4e.org', 80))
 
+text = ['H', 'o', 'L', 'A']
+output = ''
+for character in text:
+    output = output+character
+print(output)
+
