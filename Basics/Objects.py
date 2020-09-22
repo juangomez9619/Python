654ee407bb081582702c876c5bed5b7cea047f29
juangomez9619 <judgomezb@correo.udistrital.edu.co>
2020-09-21 21:14:14 -0500

    Create Objects.py

---
+++ Basics/Objects.py
@@ -0,0 +1,48 @@
+
+# capital letter
+class Dog:
+
+    def __init__(self, name, age):  # instance the object when it´s created
+        self.name = name  # attribute
+        self.age = age
+
+    def get_name(self):
+        return self.name
+
+    def get_age(self):
+        return self.age
+
+    def set_age(self, age):
+        self.age = age
+
+
+class Student:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade
+
+    def get_grade(self):
+        return self.grade
+
+
+class Course:
+    def __init__(self, name, max_students):
+        self.name = name
+        self.max_students = max_students
+        self.students = []  # create attribute with no initial value
+
+    def add_students(self, student):
+        if len(self.students) < self.max_students:
+            self.students.append(student)
+            return True
+        return False
+
+    def get_average_grade(self):
+        pass
+
+
+
+
+
+
