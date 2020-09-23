669e186a25cefcda9d3633c5ebec8c468ab92bae
juangomez9619 <judgomezb@correo.udistrital.edu.co>
2020-09-22 23:26:47 -0500

    ZooKeaper

--- Basics/Objects_2.py
+++ Basics/Objects_2.py
@@ -19,7 +19,8 @@ p1 = Person("Tim")
 p2 = Person("Juan")
 #  print(Person.number_of_people)  class method
 
-class Math: # static methods
+
+class Math:  # static methods
 
     @staticmethod
     def add5(x):
